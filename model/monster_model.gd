6937cb5d4b1cd29ff9ab2d4e53075231c45210a8
tzwm <wangjunzhe4444@gmail.com>
2021-03-21 17:44:24 +0800

    Add the Monster model

---
+++ model/monster_model.gd
@@ -0,0 +1,21 @@
+extends Node
+class_name MonsterModel
+
+export(String) var nickname
+export(Texture) var avatar
+export(int) var level
+export(int) var health_max
+export(int) var attack
+export(int) var coin_dropped
+#export(int) var skill
+
+var health_current
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
