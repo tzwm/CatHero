6aae908d2e5b5fd0227ee4e5ff06ef906a2dd5ed
tzwm <wangjunzhe4444@gmail.com>
2021-03-27 23:00:05 +0800

    Add more attributes to the CardModel

--- card.gd
+++ card.gd
@@ -12,12 +12,12 @@ func _ready():
 	pass
 
 func _on_update_card(new_card):
-	var node_before = get_node("CardModel")
-	if node_before:
-		queue_free()
-	card = new_card
-	new_card.set_name("CardModel")
-	add_child(new_card)
+#	var node_before = get_node("CardModel")
+#	if node_before:
+#		queue_free()
+#	card = new_card
+#	new_card.set_name("CardModel")
+#	add_child(new_card)
 	$Panel/VBoxContainer/TopBar/Title.text = new_card.title
 	$Panel/VBoxContainer/MarginContainer2/Description.text = new_card.description
 	
--- model/card_model.gd
+++ model/card_model.gd
@@ -3,6 +3,9 @@ class_name CardModel
 
 export(String) var title
 export(GameConst.Character) var character
+export(GameConst.CardType) var type
+export(GameConst.CardRarity) var rarity
+export(bool) var use_in_bonfire
 export(String) var description
 
 onready var attributes = $Attributes.get_children()
